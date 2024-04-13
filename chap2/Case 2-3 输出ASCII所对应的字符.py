863e76567ea8fe13ca373d3505a4361aa8c4a928
Lirving9 <Li1958160862@sina.com>
2024-04-13 21:55:11 +0800

    Learn python from Bilibili

---
+++ chap2/Case 2-3 输出ASCII所对应的字符.py
@@ -0,0 +1,8 @@
+print('b')
+print(chr(98))
+
+print(ord('北')) #使用print函数输出中文Unicode码
+print(ord('京'))
+
+print(chr(21271))
+print(chr(20140))
---
+++ chap2/Case 2-4 print将内容输出到文件中.py
@@ -0,0 +1,3 @@
+fp=open('test.txt','w')
+print('青岛！',file=fp)  #将青岛欢迎你写入test文件中
+fp.close()
